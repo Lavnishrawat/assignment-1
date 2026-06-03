a49857dba2e8c71dbc02a5b09d8b13c2c948e2ca
Lavnishrawat <rawatnaveen910@gmail.com>
2026-06-03 08:53:31 +0530

    Add Python programs for variable creation and list manipulation

--- assignment-1.py
+++ assignment-1.py
@@ -1,1 +1,29 @@
+#Assignment-1
+#Que-2:Write a python program to create three variables:name,age,city.
+name = "lavnish"
+age = 19
+city = "pilani"
+print("Name :",name)
+print("Age :",age)
+print("City :",city)
+
+#Que-3:write a python program that:Takes a user's name as input,Prints the name in uppercase,Prints the total number of characters in the name.
+name = input("Enter your name :")
+print(name.upper())
+print(len(name))
+
+#Que-5:Create a list containing the names of five fruits:Print the complete list,Print the first and last element,Print the total number of items in the list.
+fruits = ["apple","banana","papaya","litchi","grapes"]
+print("Complete fruits list :",fruits)
+print("First element :",fruits[0])
+print("Last element :",fruits[-1])
+print("Total no of fruits :",len(fruits))
+
+#Que-6: Write a Python program to:Create a list of numbers [10, 20, 30, 40, 50],Add 60 to the list,Remove 20 from the list,Print the updated list.
+numbers = [10,20,30,40,50]
+numbers.append(60)
+numbers.remove(20)
+print("Updates list :",numbers)
+
+
 
